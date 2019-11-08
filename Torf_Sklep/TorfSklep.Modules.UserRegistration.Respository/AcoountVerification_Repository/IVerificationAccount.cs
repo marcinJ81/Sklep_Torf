86617a8bed292bbf965c7529f6f6abaad5cfee3b
Marcin Juranek <m.juranek@PGG.PL>
2019-11-08 13:27:50 +0100

    Add account verification repository

---
+++ Torf_Sklep/TorfSklep.Modules.UserRegistration.Respository/AcoountVerification_Repository/IVerificationAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorfSklep.Modules.UserRegistration.Respository
+{
+    public interface IVerificationAccount
+    {
+        bool UserIsInList(int id_user);
+    }
+}
---
+++ Torf_Sklep/TorfSklep.Modules.UserRegistration.Respository/AcoountVerification_Repository/OrderVerificationAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorfSklep.Modules.UserRegistration.Respository.AcoountVerification_Repository
+{
+    public class OrderVerificationAccount : IVerificationAccount
+    {
+        public bool UserIsInList(int id_user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
