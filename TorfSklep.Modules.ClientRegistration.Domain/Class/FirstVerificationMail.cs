922f760349cd7148d5d15c54aba23606251178db
Marcin Juranek <m.juranek@PGG.PL>
2019-11-14 13:31:26 +0100

    Add empty class and folder

---
+++ TorfSklep.Modules.ClientRegistration.Domain/Class/FirstVerificationMail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorfSklep.Modules.UserRegistration.Domain
+{
+    public class FirstVerificationMail
+    {
+
+    }
+}
