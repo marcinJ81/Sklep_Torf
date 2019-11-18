bf102aae0463f6f673f304c399f5fa89b6180756
Marcin Juranek <m.juranek@PGG.PL>
2019-11-18 12:54:44 +0100

    Add class and interface for External system ID. Interface with functions AddExternalIDToList and GetNumberOfAttempts

---
+++ Torf_Sklep/TorfSklep.Modules.ExternalSystem.Repository/ExternalID.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TorfSklep.Modules.ExternalSystem.Repository
+{
+    public class ExternalID : IExternalIdFunctions
+    {
+        private string external_id;
+        private int external_numberOfAttempts;
+        private int user_id;
+
+        public ExternalID()
+        {
+            this.list_userWithEternalId = new List<ExternalID>();
+        }
+
+        public List<ExternalID> list_userWithEternalId { get; }
+
+        public void AddExternalIDToList(string external_id, int number_Attempts, int user_id)
+        {
+            this.list_userWithEternalId.Add(new ExternalID
+            {
+                external_id = external_id,
+                external_numberOfAttempts = number_Attempts,
+                user_id = user_id
+            });
+        }
+
+        public int GetNumberOfAttempts(int user_id)
+        {
+            if (!list_userWithEternalId.Any(x => x.user_id == user_id))
+            {
+                return 0;
+            }
+            return list_userWithEternalId.Where(x => x.user_id == user_id).FirstOrDefault().external_numberOfAttempts;
+        }
+    }
+
+    
+}
---
+++ Torf_Sklep/TorfSklep.Modules.ExternalSystem.Repository/IExternalIdFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorfSklep.Modules.ExternalSystem.Repository
+{
+    public interface IExternalIdFunctions
+    {
+         void AddExternalIDToList(string external_id, int number_Attempts, int user_id);
+         int GetNumberOfAttempts(int user_id);
+        List<ExternalID> list_userWithEternalId { get; }
+    }
+}
