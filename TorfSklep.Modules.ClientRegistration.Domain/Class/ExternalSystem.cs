70e0eb7a36b4eedc5765c0246318fa6e3e0691e2
Marcin Juranek <m.juranek@PGG.PL>
2019-11-20 06:36:21 +0100

    Add method SendOrderToExternalSystem in class ExternalSytemComunication and add interface

--- TorfSklep.Modules.ClientRegistration.Domain/Class/ExternalSystem.cs
+++ TorfSklep.Modules.ClientRegistration.Domain/Class/ExternalSystem.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TorfSklep.Modules.UserRegistration.Domain.Interfaces;
 
 namespace TorfSklep.Modules.UserRegistration.Domain.Class
 {
-    public class terefere
+    public class ExternalSytemComunication : IExternalSytemComunication
     {
-
+        public bool SendOrderToExternalSystem(int user_id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ TorfSklep.Modules.ClientRegistration.Domain/Interfaces/IExternalSytemComunication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorfSklep.Modules.UserRegistration.Domain.Interfaces
+{
+    public interface IExternalSytemComunication
+    {
+        bool SendOrderToExternalSystem(int user_id);
+    }
+}
