7363308d27ba9c660ed09c2af8b7a74ad41ff0b1
Marcin Juranek <m.juranek@PGG.PL>
2019-11-20 06:46:18 +0100

    Add unit test for method SendOrderToExternalSystem from class ExternalSytemComunication

---
+++ TrofSklep.Modules.ClientRegister.Domain.UnitTests/Test_UserRegistration/SendOrderToExternalSystem_Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TorfSklep.Modules.UserRegistration.Domain.Class;
+
+namespace TorfSklep.Modules.UserRegistration.Domain.UnitTests.Test_UserRegistration
+{
+   public class SendOrderToExternalSystem_Test
+    {
+        private ExternalSytemComunication externalSytemComunication;
+
+        [Test]
+        public void ShouldSendFirstTimeDataToExternalSystem_WhenNumberOfAttemptsEqualsZero()
+        {
+            //given
+
+            int user_id = 1;
+            //when
+            bool result = this.externalSytemComunication.SendOrderToExternalSystem(user_id);
+            //then
+            Assert.IsTrue(result);
+        }
+    }
+}
