80a6895060f1b681f5dfb164d30067bfb53e9c4b
Marcin Juranek <m.juranek@PGG.PL>
2019-11-25 13:40:25 +0100

    Add new unit test in repository in method AddUser class UserRepository Add Abstract class with query definition Code in test AddUser: - create table - insert one row - select all row and return list<string> In this commit use cherry-pick merge and i have error to commits from branch database_test are unmarged

---
+++ Torf_Sklep/TorfSklep.Infrastructure.DataBaseSystem.DB_sklep/IQuerySqlite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorfSklep.Infrastructure.DataBaseSystem.DB_sklep
+{
+    public interface IQuerySqlite
+    {
+        List<string> db_QueryWithoutParam_sqlConnectionAllInOne(string sqlCreateTable, string sqlInsert, string sqlSelect, int columnAmount);
+    }
+}
---
+++ Torf_Sklep/TorfSklep.Infrastructure.DataBaseSystem.DB_sklep/TestDataBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorfSklep.Infrastructure.DataBaseSystem.DB_sklep
+{
+    public class TestDataBase : IQuerySqlite
+    {
+      
+        public List<string> db_QueryWithoutParam_sqlConnectionAllInOne(string sqlCreateTable, string sqlInsert, string sqlSelect,int columnAmount)
+        {
+            SqliteConnection con = new SqliteConnection(@"DataSource=:memory:");
+            con.Open();
+
+            string query = sqlCreateTable;
+            SqliteCommand com0 = new SqliteCommand(query, con);
+            com0.ExecuteNonQuery();
+
+            Microsoft.Data.Sqlite.SqliteCommand com1 = new SqliteCommand(sqlInsert, con);
+            com1.ExecuteNonQuery();
+
+            Microsoft.Data.Sqlite.SqliteCommand com3 = new SqliteCommand(sqlSelect, con);
+            com3.ExecuteNonQuery();
+
+            List<string> result = new List<string>();
+           var rdr = com3.ExecuteReader();
+            result.Add(rdr.GetName(0) + " " + rdr.GetName(1) + " " + rdr.GetName(2));
+            while (rdr.Read())
+            {
+                result.Add(rdr.GetString(0) + " " + rdr.GetString(1) + " " + rdr.GetString(2));
+            }
+            con.Close();
+            return result;
+
+        }
+    }
+}
