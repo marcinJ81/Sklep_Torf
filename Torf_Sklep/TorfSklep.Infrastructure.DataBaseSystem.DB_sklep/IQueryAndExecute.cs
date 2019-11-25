7c422901201d98e385237cf66636056a89b32558
Marcin Juranek <m.juranek@PGG.PL>
2019-11-25 10:44:16 +0100

    Add to interface to test code in database

---
+++ Torf_Sklep/TorfSklep.Infrastructure.DataBaseSystem.DB_sklep/IQueryAndExecute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace TorfSklep.Infrastructure.DataBaseSystem.DB_sklep
+{
+   public interface IQueryAndExecute<T>
+    {
+        List<T> dbQuery_sqliteInmemory(string query);
+        List<T> dbQuery_sqliteInmemoryWithSqliteParametrs(string query, List<SqliteParameter> listOfParameters);
+        int dbQuery_sqliteInmemoryExecut4eNonQuery(string query);
+    }
+}
