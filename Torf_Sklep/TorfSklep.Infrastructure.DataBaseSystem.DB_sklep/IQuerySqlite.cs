f62c872d1637b7d76e1db6b30012e8fdba38638d
Marcin Juranek <m.juranek@PGG.PL>
2019-11-25 10:44:16 +0100

    Add to interface to test code in database cherry-pick merge between master and database_test commit first

--- Torf_Sklep/TorfSklep.Infrastructure.DataBaseSystem.DB_sklep/IQuerySqlite.cs
+++ Torf_Sklep/TorfSklep.Infrastructure.DataBaseSystem.DB_sklep/IQuerySqlite.cs
@@ -8,6 +8,10 @@ namespace TorfSklep.Infrastructure.DataBaseSystem.DB_sklep
 {
     public interface IQuerySqlite
     {
+<<<<<<< HEAD
         List<string> db_QueryWithoutParam_sqlConnectionAllInOne(string sqlCreateTable, string sqlInsert, string sqlSelect, int columnAmount);
+=======
+        List<string> db_QueryWithoutParam_sqlConnectionAllInOne(string sqlCreateTable, string sqlInsert, string sqlSelect);
+>>>>>>> 7c42290... Add to interface to test code in database
     }
 }
