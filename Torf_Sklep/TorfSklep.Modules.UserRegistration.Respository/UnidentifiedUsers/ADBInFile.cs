1765e60d0e3782f4a23943d340943219743fdf1a
Marcin Juranek <marcin.juranek2@gmail.com>
2019-12-16 11:05:00 +0100

    Add abstract class ADBInFile for  methods to operate database in file Add abstract class ADBInMemory for  methods to operate database in file Separating the file database from the database in memory

--- Torf_Sklep/TorfSklep.Modules.UserRegistration.Respository/UnidentifiedUsers/ADBInFile.cs
+++ Torf_Sklep/TorfSklep.Modules.UserRegistration.Respository/UnidentifiedUsers/ADBInFile.cs
@@ -6,36 +6,17 @@ using TorfSklep.Infrastructure.DataBaseSystem.DB_sklep;
 
 namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
 {
-    public abstract class AQueryDefinition
+    abstract class ADBInFile
     {
-        protected IQuerySqlite testDataBase;
+       protected IQuerySqlite testDataBase;
         public string createTable { get; }
         public string selectUserTable { get; }
         public string dropTable { get; }
-        private DataBaseType dbType;
-        private AQueryDefinition(DataBaseType choise)
+        public ADBInFile()
         {
-            if ((int)choise == 0)
-            {
-                this.testDataBase = new TestDataBase_InMemmory();
-            }
-            if ((int)choise == 1)
-            {
-                this.testDataBase = new TestDataBase_InFile();
-            }
-            this.dbType = choise;
-        }
-        public AQueryDefinition(TableName tableName, DataBaseType choise) :
-            this(choise)
-        {
-            if ((int)tableName == 1)
-            {
-                if ((int)choise == 1)
-                    dropTable = @"DROP TABLE IF EXISTS user_register; ";
-                else
-                    dropTable = string.Empty;
-
-                this.createTable =dropTable + @"CREATE TABLE IF NOT EXISTS user_register
+            this.testDataBase = new TestDataBase_InFile();
+            dropTable = @"DROP TABLE IF EXISTS user_register; ";
+            this.createTable = dropTable + @"CREATE TABLE IF NOT EXISTS user_register
                 (
                     user_id INTEGER IDENTITY PRIMARY KEY,
                     user_name VARCHAR NOT NULL,
@@ -46,12 +27,9 @@ namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
                     user_ban BIT NOT NULL,
                     external_id VARCHAR NOT NULL
                 );";
-                this.selectUserTable = @"select user_id, user_name,user_sname," +
-                                       "user_login,user_email, user_account_active " +
-                                       " user_ban, external_id from user_register";
-            }
-            
-
+            this.selectUserTable = @"select user_id, user_name,user_sname," +
+                                   "user_login,user_email, user_account_active " +
+                                   " user_ban, external_id from user_register";
         }
         private string getInsertQuery(User user)
         {
@@ -61,7 +39,7 @@ namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
                                 + "," + user.user_ban + "," + "'" + user.external_id + "'" + ")";
             return insertQuery;
         }
-        
+
         public bool AddUser_ToBase(User user)
         {
             Dictionary<string, string> queryDictionary = new Dictionary<string, string>();
@@ -70,30 +48,11 @@ namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
             queryDictionary.Add("Select", selectUserTable);
             var result = testDataBase.db_QueryWithoutParam_sqlConnectionAllInOne(queryDictionary);
 
-            
-            string[] source;
-            if ((int)dbType == 0)
-            {
-                User userdb = new User();
-                source = result[1].Split(new char[] { ',' });
-                userdb.user_id = int.Parse(source[0]);
-                userdb.user_name = source[1];
-                userdb.user_sname = source[2];
-                userdb.user_login = source[3];
-                userdb.user_email = source[4];
-                userdb.user_account_active = int.Parse(source[5]);
-                userdb.external_id = source[6];
-                if ((userdb.user_name == user.user_name) && (userdb.user_email == user.user_email))
-                    return true;
-                return false;
-            }
-            else
-            {
                 List<User> listUser = GetAllUsers();
                 if ((listUser.Any(x => x.user_name == user.user_name)) && (listUser.Any(x => x.user_login == user.user_login)))
                     return true;
                 return false;
-            } 
+            
         }
         public bool AccountHaveBan(int id_user)
         {
@@ -118,8 +77,8 @@ namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
         }
         public bool CheckLoginAvaible_ToBase(string loginName)
         {
-          List<User> result = GetAllUsers();
-          if (result.Any(x => x.user_login == loginName))
+            List<User> result = GetAllUsers();
+            if (result.Any(x => x.user_login == loginName))
                 return false;
             return true;
         }
@@ -187,7 +146,5 @@ namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
             });
             return listUser;
         }
-
-
     }
 }
---
+++ Torf_Sklep/TorfSklep.Modules.UserRegistration.Respository/UnidentifiedUsers/ADBInMemory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TorfSklep.Infrastructure.DataBaseSystem.DB_sklep;
+
+namespace TorfSklep.Modules.UserRegistration.Respository.UnidentifiedUsers
+{
+    public abstract class ADBInMemory
+    {
+        protected IQuerySqlite testDataBase;
+        public string createTable { get; }
+        public string selectUserTable { get; }
+    }
+}
