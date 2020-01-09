37c8cac1bb753005366a4f6abcda962385686fdf
Marcin Juranek <marcin.juranek2@gmail.com>
2020-01-09 13:44:45 +0100

    Add new project for domain  Discounts Definition Add declaration methods for domain rules

---
+++ Torf_Sklep/TorfSklep.Modules.DiscountsDefinition.Domain/DiscountsRules.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TorfSklep.Modules.DiscountsDefinition.Domain.SimpleClass;
+
+namespace TorfSklep.Modules.DiscountsDefinition.Domain
+{
+   public class DiscountsRules
+    {
+        private readonly ICreateDefinitionDiscount definitionDiscount;
+
+        public DiscountsRules(ICreateDefinitionDiscount definitionDiscount)
+        {
+            this.definitionDiscount = definitionDiscount ?? throw new ArgumentNullException(nameof(definitionDiscount));
+        }
+
+        public bool SetDiscountsRules()
+        {
+            throw new Exception();
+        }
+
+        public bool ChooseDiscountsTypes(int discount_id)
+        {
+            throw new Exception();
+        }
+
+    }
+}
