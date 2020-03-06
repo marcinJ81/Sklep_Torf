74c0f678b335679eb86b3cea18f83cf406748e7a
Marcin Juranek <marcin.juranek2@gmail.com>
2020-03-06 10:43:48 +0100

    Add repository Rebate project

---
+++ Torf_Sklep/TorfSklep.Modules.RebatesDefinitions.Repository/RebateDefinition/Rebate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorfSklep.Modules.RebatesDefinitions.Repository.RebateDefinition
+{
+   public class Rebate
+    {
+        public Rebate(int rebate_id, DateTime rebate_BeginDate, DateTime rebate_EndDate, int? rebate_Amount)
+        {
+            Rebate_id = rebate_id;
+            Rebate_BeginDate = rebate_BeginDate;
+            Rebate_EndDate = rebate_EndDate;
+            Rebate_Amount = rebate_Amount;
+        }
+
+        protected int Rebate_id { get; set; }
+        protected DateTime Rebate_BeginDate { get; set; }
+        protected DateTime Rebate_EndDate { get; set; }
+        protected int? Rebate_Amount { get; set; }
+        protected int Rebate_ValueType { get; set; }
+        protected decimal Rebate_Value { get; set; }  
+    }
+}
